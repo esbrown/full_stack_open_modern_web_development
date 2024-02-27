cf5e8f95cd6ffbbd6338e4d53e656d74c39e8543
Ethan Brown <ethansbrown711@gmail.com>
2024-02-27 09:39:13 +0530

    3.14 connect add person to backend

--- part3/index.js
+++ part3/index.js
@@ -98,15 +98,14 @@ app.post("/api/persons", (request, response) => {
     })
   }
 
-  const person = {
-    id: generateId(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  }
+  })
 
-  persons = persons.concat(person)
-
-  response.json(person)
+  person.save().then((savedPerson) => {
+    response.json(savedPerson)
+  })
 })
 
 const unknownEndpoint = (request, response) => {
