67f2c1816f3184bc0faecdd692f425bd5298f785
Ethan Brown <ethansbrown711@gmail.com>
2024-02-28 15:39:55 +0530

    4.2 complete, refactoring

---
+++ part4/app.js
@@ -0,0 +1,29 @@
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = config.MONGODB_URI
+
+logger.info('connecting to', url)
+
+mongoose
+  .connect(url)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
