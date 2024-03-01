264d4798489bd3bdf39d9f5bd85c76b28ca3d964
Ethan Brown <ethansbrown711@gmail.com>
2024-03-01 12:49:08 +0530

    4.20 add token extractor middleware

--- part4/app.js
+++ part4/app.js
@@ -9,6 +9,7 @@ const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 const mongoose = require('mongoose')
 const errorHandler = require('./utils/error_handler')
+const tokenExtractor = require('./utils/token_extractor')
 
 mongoose.set('strictQuery', false)
 
@@ -28,6 +29,7 @@ mongoose
 app.use(cors())
 app.use(express.json())
 
+app.use(tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
