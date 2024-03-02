6983cf5581b2f11080453eca847343108c007b6e
Ethan Brown <ethansbrown711@gmail.com>
2024-03-02 12:45:14 +0530

    5.2 persist logged in user and add log out button

--- part5/src/App.jsx
+++ part5/src/App.jsx
@@ -5,6 +5,8 @@ import ErrorMessage from './components/ErrorMessage'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
+const userLocalStorageKey = 'loggedInUser'
+
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
@@ -20,6 +22,8 @@ const App = () => {
         username,
         password,
       })
+
+      window.localStorage.setItem(userLocalStorageKey, JSON.stringify(user))
       setUser(user)
       setUsername('')
       setPassword('')
@@ -35,22 +39,38 @@ const App = () => {
     blogService.getAll().then((blogs) => setBlogs(blogs))
   }, [])
 
+  useEffect(() => {
+    const loggedInUserJSON = window.localStorage.getItem(userLocalStorageKey)
+    if (loggedInUserJSON) {
+      const user = JSON.parse(loggedInUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   return (
     <div>
       <ErrorMessage message={errorMessage} />
 
       {user === null ? (
-        <LoginForm
-          username={username}
-          password={password}
-          setUsername={setUsername}
-          setPassword={setPassword}
-          handleLogin={handleLogin}
-        />
+        <div>
+          <h2>Log in to application</h2>
+          <LoginForm
+            username={username}
+            password={password}
+            setUsername={setUsername}
+            setPassword={setPassword}
+            handleLogin={handleLogin}
+          />
+        </div>
       ) : (
         <div>
-          <p>{user.name} logged-in</p>
           <h2>blogs</h2>
+          <p>{user.name} logged-in</p>
+          <button
+            onClick={() => window.localStorage.removeItem(userLocalStorageKey)}
+          >
+            log out
+          </button>
           {blogs.map((blog) => (
             <Blog key={blog.id} blog={blog} />
           ))}
