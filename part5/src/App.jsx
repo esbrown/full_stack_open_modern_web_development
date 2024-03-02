7f219e2eb4326bb087bf742b5b18576dc453fdf1
Ethan Brown <ethansbrown711@gmail.com>
2024-03-02 13:10:16 +0530

    5.3 and 5.4, allow logged in user to add blog, show success and errors

--- part5/src/App.jsx
+++ part5/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import LoginForm from './components/LoginForm'
+import SuccessMessage from './components/SuccessMessage'
 import ErrorMessage from './components/ErrorMessage'
 import blogService from './services/blogs'
 import loginService from './services/login'
@@ -13,6 +14,10 @@ const App = () => {
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
+  const [successMessage, setSuccessMessage] = useState(null)
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -22,17 +27,20 @@ const App = () => {
         username,
         password,
       })
-
+      blogService.setToken(user.token)
       window.localStorage.setItem(userLocalStorageKey, JSON.stringify(user))
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (exception) {
-      setErrorMessage('Wrong credentials')
-      setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)
+      updateErrorMessage('Username or password is incorrect')
     }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem(userLocalStorageKey)
+    blogService.setToken('')
+    window.location.reload()
   }
 
   useEffect(() => {
@@ -43,12 +51,80 @@ const App = () => {
     const loggedInUserJSON = window.localStorage.getItem(userLocalStorageKey)
     if (loggedInUserJSON) {
       const user = JSON.parse(loggedInUserJSON)
+      blogService.setToken(user.token)
       setUser(user)
     }
   }, [])
 
+  const updateSuccessMessage = (message) => {
+    setSuccessMessage(message)
+    setTimeout(() => {
+      setSuccessMessage(null)
+    }, 5000)
+  }
+
+  const updateErrorMessage = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 5000)
+  }
+
+  const handleBlogSubmit = async (event) => {
+    event.preventDefault()
+    try {
+      const newBlog = {
+        title: title,
+        author: author,
+        url: url,
+      }
+      const response = await blogService.create(newBlog)
+      updateSuccessMessage(`New blog "${title}" successfully created`)
+      console.log(response)
+    } catch (exception) {
+      updateErrorMessage(
+        `There was an issue creating your new blog: ${exception.message}`
+      )
+      console.log('error', exception)
+    }
+  }
+
+  const blogForm = () => (
+    <form onSubmit={handleBlogSubmit}>
+      <div>
+        title{' '}
+        <input
+          type='text'
+          value={title}
+          name='Title'
+          onChange={({ target }) => setTitle(target.value)}
+        />
+      </div>
+      <div>
+        author{' '}
+        <input
+          type='text'
+          value={author}
+          name='Author'
+          onChange={({ target }) => setAuthor(target.value)}
+        />
+      </div>
+      <div>
+        url{' '}
+        <input
+          type='text'
+          value={url}
+          name='Url'
+          onChange={({ target }) => setUrl(target.value)}
+        />
+      </div>
+      <button type='submit'>create</button>
+    </form>
+  )
+
   return (
     <div>
+      <SuccessMessage message={successMessage} />
       <ErrorMessage message={errorMessage} />
 
       {user === null ? (
@@ -65,12 +141,12 @@ const App = () => {
       ) : (
         <div>
           <h2>blogs</h2>
-          <p>{user.name} logged-in</p>
-          <button
-            onClick={() => window.localStorage.removeItem(userLocalStorageKey)}
-          >
-            log out
-          </button>
+          <div>
+            {user.name} logged-in{' '}
+            <button onClick={handleLogout}>log out</button>
+          </div>
+          <h2>Create new</h2>
+          {blogForm()}
           {blogs.map((blog) => (
             <Blog key={blog.id} blog={blog} />
           ))}
