2929256305b77f2a88e00b21a01853d8f5bc5f71
Ethan Brown <ethansbrown711@gmail.com>
2024-03-03 14:49:46 +0530

    5.7 add Blog details and togglablity

--- part5/src/components/Blog.jsx
+++ part5/src/components/Blog.jsx
@@ -1,7 +1,39 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  const [detailsVisible, setDetailsVisible] = useState(false)
+
+  const hideWhenVisible = { display: detailsVisible ? 'none' : '' }
+  const showWhenVisible = { display: detailsVisible ? '' : 'none' }
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenVisible}>
+        {blog.title}{' '}
+        <button onClick={() => setDetailsVisible(true)}>view</button>
+      </div>
+      <div style={showWhenVisible}>
+        <div>
+          {blog.title}{' '}
+          <button onClick={() => setDetailsVisible(false)}>hide</button>
+        </div>
+        <div>{blog.url}</div>
+        <div>
+          likes {blog.likes}{' '}
+          <button onClick={() => console.log('like clicked')}>like</button>
+        </div>
+        <div>{blog.author}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
