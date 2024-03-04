e5f4c323f787836086117601e77466af6c7af858
Ethan Brown <ethansbrown711@gmail.com>
2024-03-04 10:38:13 +0530

    5.14 test like button triggers handler

--- part5/src/components/Blog.test.js
+++ part5/src/components/Blog.test.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
   let container
+  let mockHandler
+
   const blog = {
     title: 'title_1',
     author: 'author_1',
@@ -18,7 +21,8 @@ describe('<Blog />', () => {
   }
 
   beforeEach(() => {
-    container = render(<Blog blog={blog} />).container
+    mockHandler = jest.fn()
+    container = render(<Blog blog={blog} updateLikes={mockHandler} />).container
   })
 
   test('at start the children are not displayed', async () => {
@@ -26,5 +30,25 @@ describe('<Blog />', () => {
     expect(shown).not.toHaveStyle('display: none')
     const hidden = container.querySelector('.hiddenByDefault')
     expect(hidden).toHaveStyle('display: none')
+  })
+
+  test('after clicking the button, children are displayed', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const shown = container.querySelector('.shownByDefault')
+    expect(shown).toHaveStyle('display: none')
+    const hidden = container.querySelector('.hiddenByDefault')
+    expect(hidden).not.toHaveStyle('display: none')
+  })
+
+  test('clicking the button twice event handler twice', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('like')
+    await user.click(button)
+    await user.click(button)
+
+    expect(mockHandler.mock.calls).toHaveLength(2)
   })
 })
